197dafbfac472297e00b30de8d09f318625717cc
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-05-02 13:21:29 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int n=5;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print(" "+j);
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
