6ad9d0a1a93f12b6f406a4f5b02ec3766423ecb8
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-05-07 08:05:00 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,25 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Pattern7{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number of row");
+		int n= new Scanner(System.in).nextInt();
+
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				if(i==1||i==n||j==1||j==n)
+				System.out.print("*");
+				
+				else
+					System.out.print(" ");
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
