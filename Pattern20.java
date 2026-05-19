845ebe1ac54e17118b54774a4cc8f908727d6889
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-05-19 17:14:06 +0530

    Add files via upload

---
+++ Pattern20.java
@@ -0,0 +1,22 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Pattern20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("enter number of row");
+	int n= new Scanner(System.in).nextInt();
+
+
+	for(int i=n;i>=1;i--) {
+		for(int j=i;j>=1;j--) {
+			System.out.print(i);
+		}
+		System.out.println();
+	}
+}
+
+}
