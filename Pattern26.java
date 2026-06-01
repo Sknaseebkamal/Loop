5c71a1e32be8ca21260c670955f78c3a4ed6c61d
Sknaseebkamal <sknaseebkamal@gmail.com>
2026-06-01 06:23:33 +0530

    Add files via upload

---
+++ Pattern26.java
@@ -0,0 +1,22 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Pattern26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number of row");
+		int n= new Scanner(System.in).nextInt();
+
+		for(int i=1;i<=n;i++) {
+			for(int k=1;k<=n-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
